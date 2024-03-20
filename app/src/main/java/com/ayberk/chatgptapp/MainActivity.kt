c447772e3fa52b08d1e00f3efcfec161fe6a6d88
ayberktmn <ayberktemin@gmail.com>
2024-03-20 17:05:19 +0300

    chatgpt added

--- app/src/main/java/com/ayberk/chatgptapp/MainActivity.kt
+++ app/src/main/java/com/ayberk/chatgptapp/MainActivity.kt
@@ -1,11 +1,93 @@
 package com.ayberk.chatgptapp
 
+import android.app.VoiceInteractor
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.inputmethod.EditorInfo
+import android.widget.TextView
+import android.widget.Toast
+import com.ayberk.chatgptapp.databinding.ActivityMainBinding
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.RequestBody.Companion.toRequestBody
+import okhttp3.Response
+import org.json.JSONArray
+import org.json.JSONObject
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val client = OkHttpClient()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding. etQuestion.setOnEditorActionListener(TextView.OnEditorActionListener { v, actionId, event ->
+            if (actionId == EditorInfo.IME_ACTION_SEND) {
+
+                // setting response tv on below line.
+                binding.txtResponse.text = "Please wait.."
+
+                // validating text
+                val question = binding.etQuestion.text.toString().trim()
+                Toast.makeText(this,question, Toast.LENGTH_SHORT).show()
+                if(question.isNotEmpty()){
+                    getResponse(question) { response ->
+                        runOnUiThread {
+                            binding.txtResponse.text = response
+                        }
+                    }
+                }
+                return@OnEditorActionListener true
+            }
+            false
+        })
+    }
+
+    fun getResponse(question: String, callback: (String) -> Unit){
+        val apiKey = "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"
+        val url = "https://api.openai.com/v1/completions"
+
+        val requestBody="""
+            {
+            "model": "gpt-3.5-turbo-instruct",
+            "prompt": "$question",
+            "max_tokens": 500,
+            "temperature": 0
+            }
+        """.trimIndent()
+
+        val request = Request.Builder()
+            .url(url)
+            .addHeader("Content-Type", "application/json")
+            .addHeader("Authorization", "Bearer $apiKey")
+            .post(requestBody.toRequestBody("application/json".toMediaTypeOrNull()))
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("Request Failure", "API request failed", e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val body=response.body?.string()
+                if (body != null) {
+                    Log.v("data",body)
+                }
+                else{
+                    Log.v("data","empty")
+                }
+                val jsonObject= JSONObject(body)
+                val jsonArray: JSONArray =jsonObject.getJSONArray("choices")
+                val textResult=jsonArray.getJSONObject(0).getString("text")
+                callback(textResult)
+            }
+        })
     }
 }
